afe2395edc1b07dd10014fbd1263ee37b4a2fec2
Jeanlui21 <jean.ferrer@tecsup.edu.pe>
2019-09-03 17:13:06 -0500

    quinto

--- Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
@@ -1,9 +1,9 @@
 package com.tecsup.lab01.model;
 
 public class User {
-	public String nombre = "Francisco";
-	public String apellido_pat = "Ramirez";
-	public int edad = 20;
+	public String nombre = "Gianluca";
+	public String apellido_pat = "Gave Balarezo";
+	public int edad = 21;
 
 
 
