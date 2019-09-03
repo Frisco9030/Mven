023afaf6e2a9c9c1e19747b2639b181dd98f5a44
Francisco <frisco9030@gmail.com>
2019-09-03 17:20:02 -0500

    Octavo :)

--- Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/controller/UserController.java
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.tecsup.lab01.controller;
+import com.tecsup.lab01.model.*;
 
 public class UserController {
+User f = new User();
 
+public void nombre_completo(){
+System.out.println("Nombre : "+f.nombre + " " + f.apellido_pat);
+System.out.println("Edad: "+ f.edad);
+}
 }
--- Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,7 @@
 package com.tecsup.lab01.model;
 
 public class User {
-
+    public String nombre = "Francisco";
+    public String apellido_pat = "Ramirez Gomero";
+    public int edad = 25;
 }
