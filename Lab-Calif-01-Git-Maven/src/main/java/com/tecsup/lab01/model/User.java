598e131f03673923995afa22fb84679fb41063b9
Jeanlui21 <jean.ferrer@tecsup.edu.pe>
2019-09-03 16:59:39 -0500

    tercero

--- Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,11 @@
 package com.tecsup.lab01.model;
 
 public class User {
+	public String nombre = "Francisco";
+	public String apellido_pat = "Ramirez";
+	public int edad = 20;
+
+
 
 }
+
--- Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/view/UserView.java
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab01/view/UserView.java
@@ -1,5 +1,16 @@
 package com.tecsup.lab01.view;
+import com.tecsup.lab01.controller.*;
 
 public class UserView {
 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserController n = new UserController();
+		
+		n.nombre_completo();
+		
+
+	}
 }
+
